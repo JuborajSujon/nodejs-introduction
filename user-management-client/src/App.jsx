0ff853b76536cf9a64bf6818d653b7fded42cb12
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-19 23:16:46 +0600

    post req send to server side

--- user-management-client/src/App.jsx
+++ user-management-client/src/App.jsx
@@ -10,10 +10,57 @@ function App() {
       .then((data) => setUser(data));
   }, []);
 
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const formUser = { name, email };
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(formUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUser = [...user, data];
+        setUser(newUser);
+        form.reset();
+      });
+  };
+
   return (
     <>
       <h1>User Management System</h1>
       <h3>Total Users: {user.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" />
+        <br />
+        <input type="email" name="email" placeholder="Email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <table className={{ border: "1px solid black" }}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {user.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
--- user-management-server/index.js
+++ user-management-server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const users = [
   { id: 1, name: "John", email: "one@one.com" },
@@ -14,6 +19,13 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
