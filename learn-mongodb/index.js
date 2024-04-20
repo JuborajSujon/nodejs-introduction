11536c362f9455e57119a56f731c102f6ac371a9
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-20 22:42:21 +0600

    mongoose connect with mongodb by async function

--- learn-mongodb/index.js
+++ learn-mongodb/index.js
@@ -6,13 +6,24 @@ const app = express();
 const port = 3000;
 
 // Connect to MongoDB
-mongoose
-  .connect("mongodb://127.0.0.1:27017/learn-mongodb")
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => {
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/learn-mongodb")
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => {
+//     console.log(err);
+//     process.exit(1);
+//   });
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/learn-mongodb");
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log("MongoDB connection failed");
     console.log(err);
     process.exit(1);
-  });
+  }
+};
 
 app.use(express.json());
 // Enable all CORS requests
@@ -22,6 +33,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Example app listening at http://localhost:${port}`);
+  await connectDB();
 });
