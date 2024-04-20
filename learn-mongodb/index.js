213ecf7ad99fde3bc235ecbf742de1b06c6509ee
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-20 22:37:30 +0600

    define connection method by promise

--- learn-mongodb/index.js
+++ learn-mongodb/index.js
@@ -1,7 +1,23 @@
-import express from "express";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/learn-mongodb")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
+app.use(express.json());
+// Enable all CORS requests
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
