ce7b972c46f0b0cc787b4e2cac31ff3c8790ea5d
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-20 10:08:10 +0600

    user create and send to mongo db to post by express

--- simple-crud-client/src/App.jsx
+++ simple-crud-client/src/App.jsx
@@ -1,9 +1,39 @@
 import "./App.css";
 
 function App() {
+  const handleAddUser = async (event) => {
+    try {
+      event.preventDefault();
+      const form = event.target;
+      const name = form.name.value;
+      const email = form.email.value;
+      const formUser = { name, email };
+      const url = "http://localhost:5000/users";
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(formUser),
+      });
+      const data = await response.json();
+      console.log(data);
+      form.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" />
+        <br />
+        <input type="email" name="email" placeholder="Email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   );
 }
