af65e5445e6d0c7678cada674b27231d1d1f720d
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-22 19:13:51 +0600

    Get single product

--- coffee-store/coffee-store-client/src/components/CoffeeCard/CoffeeCard.jsx
+++ coffee-store/coffee-store-client/src/components/CoffeeCard/CoffeeCard.jsx
@@ -1,7 +1,44 @@
+import Swal from "sweetalert2";
+
 const CoffeeCard = ({ coffee }) => {
-  console.log(coffee);
-  const { coffee_name, quantity, supplier, taste, category, details, photo } =
-    coffee;
+  const {
+    _id,
+    coffee_name,
+    quantity,
+    supplier,
+    taste,
+    category,
+    details,
+    photo,
+  } = coffee;
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="card card-side bg-base-100 shadow-xl">
       <figure>
@@ -32,7 +69,9 @@ const CoffeeCard = ({ coffee }) => {
         <div className="card-actions justify-end">
           <button className="btn btn-primary">View</button>
           <button className="btn btn-primary">Edit</button>
-          <button className="btn btn-primary">Delete</button>
+          <button onClick={() => handleDelete(_id)} className="btn btn-accent">
+            Delete
+          </button>
         </div>
       </div>
     </div>
--- coffee-store/coffee-store-server/index.js
+++ coffee-store/coffee-store-server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -42,6 +42,47 @@ async function run() {
     app.get("/coffee", async (req, res) => {
       const cursor = coffeeCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Get a single document
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Delete a document
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Update a document
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedCoffee = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          coffee_name: updatedCoffee.coffee_name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo,
+        },
+      };
+      const result = await coffeeCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
       res.send(result);
     });
 
