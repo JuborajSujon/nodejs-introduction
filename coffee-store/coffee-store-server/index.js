0b84c079c75d6ef144671160dd8013e9f9fa93e3
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-22 11:51:49 +0600

    Coffee store new project initialization with basic setup

---
+++ coffee-store/coffee-store-server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Coffee Store Server Running");
+});
+
+app.listen(port, () => {
+  console.log(`coffe store server listening at http://localhost:${port}`);
+});
